4ceb71acdd566759e77ca7a9e81d97eaf1af6fb5
Nam Nguyen <namhnguy@gmail.com>
2022-03-02 20:11:31 -0600

    Updates

---
+++ precache-manifest.27327cc87852e6f56f89ca5c47f72489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4235dbcc4134fafbc46d5076f6270e8f",
+    "url": "/namhnguy/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "4b8eadec0f1984a6fac7",
+    "url": "/namhnguy/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5485b2ea608b0e7ae9e0",
+    "url": "/namhnguy/react-frontend-dev-portfolio/static/css/main.77063693.chunk.css"
+  },
+  {
+    "revision": "4b8eadec0f1984a6fac7",
+    "url": "/namhnguy/react-frontend-dev-portfolio/static/js/2.dce90a18.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/namhnguy/react-frontend-dev-portfolio/static/js/2.dce90a18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5485b2ea608b0e7ae9e0",
+    "url": "/namhnguy/react-frontend-dev-portfolio/static/js/main.0c4ee2cd.chunk.js"
+  },
+  {
+    "revision": "a3c5ce5d7137d91a6497",
+    "url": "/namhnguy/react-frontend-dev-portfolio/static/js/runtime-main.1afddaa2.js"
+  }
+]);
