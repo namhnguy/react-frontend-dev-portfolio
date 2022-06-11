b8acf95f4e336bd78c44e5667729407019583eed
Nam Nguyen <namhnguy@gmail.com>
2022-06-11 15:17:09 -0500

    update info

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import { Icon } from "@iconify/react";
-import reactIcon from "@iconify/icons-logos/react";
-import javascriptIcon from "@iconify/icons-logos/javascript";
-import unityIcon from "@iconify/icons-logos/unity";
-import cSharp from "@iconify/icons-logos/c-sharp";
+import React, { Component } from 'react';
+import { Icon } from '@iconify/react';
+import reactIcon from '@iconify/icons-logos/react';
+import javascriptIcon from '@iconify/icons-logos/javascript';
+import unityIcon from '@iconify/icons-logos/unity';
+import cSharp from '@iconify/icons-logos/c-sharp';
+import javaIcon from '@iconify/icons-logos/java';
 
 class About extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
-      var profilepic = "images/" + this.props.sharedBasicInfo.image;
+      var profilepic = 'images/' + this.props.sharedBasicInfo.image;
     }
     if (this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.about;
@@ -17,72 +18,76 @@ class About extends Component {
     }
 
     return (
-      <section id="about">
-        <div className="col-md-12">
-          <h1 style={{ color: "black" }}>
+      <section id='about'>
+        <div className='col-md-12'>
+          <h1 style={{ color: 'black' }}>
             <span>{sectionName}</span>
           </h1>
-          <div className="row center mx-auto mb-5">
-            <div className="col-md-4 mb-5 center">
-              <div className="polaroid">
-                <span style={{ cursor: "auto" }}>
+          <div className='row center mx-auto mb-5'>
+            <div className='col-md-4 mb-5 center'>
+              <div className='polaroid'>
+                <span style={{ cursor: 'auto' }}>
                   <img
-                    height="250px"
+                    height='250px'
                     src={profilepic}
-                    alt="Avatar placeholder"
+                    alt='Avatar placeholder'
                   />
                   <Icon
                     icon={javascriptIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
                   />
                   <Icon
                     icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
                   />
                   <Icon
                     icon={cSharp}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
                   />
                   <Icon
                     icon={unityIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
+                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
+                  />
+                  <Icon
+                    icon={javaIcon}
+                    style={{ fontSize: '400%', margin: '9% 5% 0 5%' }}
                   />
                 </span>
               </div>
             </div>
 
-            <div className="col-md-8 center">
-              <div className="col-md-10">
-                <div className="card">
-                  <div className="card-header">
+            <div className='col-md-8 center'>
+              <div className='col-md-10'>
+                <div className='card'>
+                  <div className='card-header'>
                     <span
-                      className="iconify"
-                      data-icon="emojione:red-circle"
-                      data-inline="false"
-                    ></span>{" "}
-                    &nbsp;{" "}
+                      className='iconify'
+                      data-icon='emojione:red-circle'
+                      data-inline='false'
+                    ></span>{' '}
+                    &nbsp;{' '}
                     <span
-                      className="iconify"
-                      data-icon="twemoji:yellow-circle"
-                      data-inline="false"
-                    ></span>{" "}
-                    &nbsp;{" "}
+                      className='iconify'
+                      data-icon='twemoji:yellow-circle'
+                      data-inline='false'
+                    ></span>{' '}
+                    &nbsp;{' '}
                     <span
-                      className="iconify"
-                      data-icon="twemoji:green-circle"
-                      data-inline="false"
+                      className='iconify'
+                      data-icon='twemoji:green-circle'
+                      data-inline='false'
                     ></span>
                   </div>
                   <div
-                    className="card-body font-trebuchet text-justify ml-3 mr-3"
+                    className='card-body font-trebuchet text-justify ml-3 mr-3'
                     style={{
-                      height: "auto",
-                      fontSize: "132%",
-                      lineHeight: "200%",
+                      height: 'auto',
+                      fontSize: '132%',
+                      lineHeight: '200%',
                     }}
                   >
                     <br />
-                    <span className="wave">{hello}</span>
+                    <span className='wave'>{hello}</span>
                     <br />
                     <br />
                     {about}
